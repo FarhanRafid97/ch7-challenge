f340082efcac332d84544c9c41755c4d3243df44
farhan <farhanrafid97@gmail.com>
2022-06-11 17:10:53 +0700

    add midleware to fighroom

--- route/routeGame.js
+++ route/routeGame.js
@@ -2,8 +2,9 @@ const express = require('express');
 const { playGame, createRoom } = require('../controllers/gameController');
 
 const routeGame = express.Router();
+const isLoggedUser = require('../middleware/isLoggedUser');
 
 routeGame.post('/create-room', createRoom);
-routeGame.post('/play-game/:id', playGame);
+routeGame.post('/play-game/:id', isLoggedUser, playGame);
 
 module.exports = routeGame;
